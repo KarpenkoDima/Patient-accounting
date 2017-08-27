6534460f9f819f19723b71be3d7b9ff91e6ddd7b
KarpenkoDima <dimka2010@gmail.com>
2017-08-27 17:26:27 +0300

    Connection manager for database.

---
+++ PatientAccounting/SOPB.DAL/ConnectionManager/ConnectionManager.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace SOPB.Accounting.DAL.ConnectionManager
+{
+    /// <summary>
+    /// Repository for PatientAccounting connection settings
+    /// </summary>
+    public static class ConnectionManager
+    {
+        // cache data for connection settings.
+        private static string _dbProviderName;
+        private static string _dbDatabaseName;
+        private static string _dbServerName;
+
+        private static string _connectionString;
+        private static SqlConnection _sqlConnection;
+
+        static ConnectionManager()
+        {
+            _dbDatabaseName = ConfigurationManager.AppSettings["DatabaseName"];
+            _dbServerName = ConfigurationManager.AppSettings["ServerName"];
+            _dbProviderName = ConfigurationManager.ConnectionStrings["SOPB.PatientAccounting"].ProviderName;
+            _connectionString = String.Empty;
+            _sqlConnection = new SqlConnection();
+        }
+
+        public static string DbProviderName
+        {
+            get { return ConfigurationManager.ConnectionStrings["SOPB.PatientAcounting"].ProviderName; }
+        }
+
+        public static string ConnectionString
+        {
+            get { return _connectionString; }
+        }
+
+        public static SqlConnection GetConnection(string login, string password)
+        {
+            SetConnection(login, password);
+            if (string.IsNullOrEmpty(_connectionString))
+            {
+                throw new ArgumentNullException("Class Connection Manager","Незадана строка подключения к БД. Вызовите для начала метод SetConnectionString.");
+            }
+            return _sqlConnection;
+        }
+
+        private static void SetConnection(string login, string password)
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = _dbServerName;
+            builder.InitialCatalog = _dbDatabaseName;
+            builder.UserID = login;
+            builder.Password = password;
+            builder.MultipleActiveResultSets = true;
+            _connectionString = builder.ConnectionString;
+            if (string.IsNullOrEmpty(_sqlConnection.ConnectionString))
+            {
+                _sqlConnection.ConnectionString = _connectionString;
+            }
+            else
+            {
+                _sqlConnection = new SqlConnection(_connectionString);
+            }
+        }
+
+        public static bool TestConnection(string login, string password)
+        {
+            try
+            {
+                using (SqlConnection connection = GetConnection(login, password))
+                {
+                    connection.Open();
+                    return connection.State == ConnectionState.Open;
+                }
+            }
+            catch (SqlException sqlException)
+            {
+                throw sqlException;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ PatientAccounting/SOPB.DALUnitTest/ConnectionManager/ConnectionManagerTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SOPB.Accounting.DAL.ConnectionManager;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOPB.Accounting.DAL.ConnectionManager.Tests
+{
+    [TestClass()]
+    public class ConnectionManagerTests
+    {
+        [TestMethod()]
+        public void GetConnectionSuccessTest()
+        {
+            SqlConnection conn = ConnectionManager.GetConnection("Катя", "1");
+            Assert.AreNotEqual(conn.ConnectionString, string.Empty);
+        }
+
+        [TestMethod()]
+        public void TestConnectionTest()
+        {
+            Assert.IsTrue(ConnectionManager.TestConnection("Катя", "1"));
+        }
+        [TestMethod()]
+        public void OpenConnectionTest()
+        {
+            SqlConnection conn = ConnectionManager.GetConnection("Катя", "1");
+            conn.Open();
+            Assert.IsTrue(conn.State == ConnectionState.Open);
+        }
+        [TestMethod()]
+        public void OpenCloseConnectionAnyMoreTest()
+        {
+            SqlConnection conn = ConnectionManager.GetConnection("Катя", "1");
+            
+            int i = 10;
+            while (--i > 0)
+            {
+                conn.Open();
+                Assert.IsTrue(conn.State == ConnectionState.Open);
+
+                conn.Close();
+                Assert.IsTrue(conn.State == ConnectionState.Closed);
+            }
+        }
+    }
+}
