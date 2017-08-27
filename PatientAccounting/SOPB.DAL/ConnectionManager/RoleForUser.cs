896e0e8d6b41bc0b48271356b7fe7642f3ff526d
KarpenkoDima <dimka2010@gmail.com>
2017-08-27 19:33:45 +0300

    What are you? Get the role for current user.

---
+++ PatientAccounting/SOPB.DAL/ConnectionManager/RoleForUser.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace SOPB.Accounting.DAL.ConnectionManager
+{
+    /// <summary>
+    /// What are You?
+    /// </summary>
+    public static class RoleForUser
+    {
+        public static string GetRoleForUser(string login, string password)
+        {
+            string role = String.Empty;
+            try
+            {
+                SqlConnection connection = ConnectionManager.GetConnection(login, password);
+                
+
+                using (SqlCommand command = new SqlCommand(connection.ConnectionString, connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.CommandText = "uspGetRoleForUser";
+                    SqlParameter parameter = new SqlParameter
+                    {
+                        ParameterName = "@role",
+                        Size = 50,
+                        Direction = ParameterDirection.Output
+                    };
+                    command.Parameters.Add(parameter);
+
+                    command.Connection.Open();
+                    command.ExecuteNonQuery();
+                    role = ((string) command.Parameters["@role"].Value).Trim();
+                }
+            }
+            catch (SqlException)
+            {
+
+                throw;
+            }
+            catch (InvalidOperationException)
+            {
+                throw;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return role;
+        }
+    }
+}
